034ed34932835614a4b6758b99927ba0ad72299b
LasseWardenaer <lassetw@stud.ntnu.no>
2020-02-11 18:25:48 +0100

    Added folders

---
+++ oppgave/project_folder/source/enums.h
@@ -0,0 +1,33 @@
+enum floor{
+    floor_1 = 1,
+    floor_2 = 2,
+    floor_3 = 3,
+    floor_4 = 4
+}
+
+enum direction {
+    UP,
+    DOWN
+}
+
+enum elevator_state{
+    initilize,
+    idle,
+    move,
+    door_open,
+    emergency_stop
+}
+
+enum light{
+    floor_up_light,
+    floor_down_light,
+    elev_floor_1_light,
+    elev_floor_2_light,
+    elev_floor_3_light,
+    elev_floor_4_light,
+    floor_1_light,
+    floor_2_light,
+    floor_3_light,
+    floor_4_light
+    stop_light
+}
